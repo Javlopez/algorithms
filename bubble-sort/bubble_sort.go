86864ff6da4dd8af95ef5c5b91150589111bbb61
Javier <sjavierlopez@gmail.com>
2021-01-22 07:16:49 -0600

    Bubble Sort

--- bubble-sort/bubble_sort.go
+++ bubble-sort/bubble_sort.go
@@ -5,18 +5,22 @@ import "fmt"
 func BubbleSort(numbers []int) []int  {
 
 	for i := 0; i<len(numbers); i++ {
-		numbers = sweep(numbers)
+		numbers, didSwap := sweep(numbers, i)
+		if !didSwap {
+			return numbers
+		}
 		fmt.Printf("sort phase: %d - %+v\n", i, numbers)
 	}
 	return numbers
 }
 
-func sweep(numbers []int) []int {
+func sweep(numbers []int, previousPhases int) ([]int, bool) {
 	n  := len(numbers)
 	first_index := 0
 	second_index := 1
+	didSwap := false
 
-	for second_index < n {
+	for second_index < (n - previousPhases) {
 
 		firstNumber := numbers[first_index]
 		secondNumber := numbers[second_index]
@@ -24,10 +28,11 @@ func sweep(numbers []int) []int {
 			//swap numbers
 			numbers[first_index] = secondNumber
 			numbers[second_index] = firstNumber
+			didSwap = true
 		}
 
 		first_index++
 		second_index++
 	}
-	return numbers
+	return numbers, didSwap
 }
--- bubble-sort/bubble_sort_test.go
+++ bubble-sort/bubble_sort_test.go
@@ -8,8 +8,8 @@ import (
 func TestBubbleSort(t *testing.T) {
 
 	t.Run("Simple bubble sort", func(t *testing.T) {
-		input := []int{7, 6, 4, 5, 3, 2}
-		expected := []int{2, 3, 4, 5, 6, 7}
+		input := []int{7, 6, 4, 5, 10, 3, 2,1}
+		expected := []int{1, 2, 3, 4, 5, 6, 7, 10}
 		actual := BubbleSort(input)
 		assert.Equal(t, expected, actual)
 	})
